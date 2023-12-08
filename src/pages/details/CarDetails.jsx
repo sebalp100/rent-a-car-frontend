96380151fa223f4373a4a1984fa80b63483ad5fa
sebalp100 <sebalp100@gmail.com>
2023-12-08 15:14:40 -0300

    Edit icon divs

--- src/pages/details/CarDetails.jsx
+++ src/pages/details/CarDetails.jsx
@@ -66,33 +66,27 @@ const CarDetails = ({ user }) => {
                       alt={`${details.model} Picture`}
                       className="shadow"
                     />
-                    <div className="flex pt-6 text-lg justify-between">
-                      <div className="flex flex-col sm:flex-row sm:gap-[45%] justify-between">
-                        <h3
-                          className="flex gap-1 items-center"
-                          title={`Transmission: ${details.engine}`}
-                        >
-                          <IoMdSettings className="text-xl" />
-                          {details.engine}
-                        </h3>
-                        <h3 className="flex gap-1 items-center" title="CC">
-                          <FaCubes className="text-xl" />
-                          {details.cc}
-                        </h3>
-                      </div>
-                      <div className="flex flex-col sm:flex-row sm:gap-[45%] justify-between">
-                        <h3
-                          className="flex gap-1 items-center"
-                          title="Top Speed"
-                        >
-                          <BsSpeedometer2 className="text-xl" />
-                          {details.top_speed} Km/h
-                        </h3>
-                        <h3 className="flex gap-1 items-center" title="Mileage">
-                          <GiPathDistance className="text-xl" />
-                          {details.mileage} Km
-                        </h3>
-                      </div>
+                    <div className="flex pt-6 text-lg flex-wrap justify-between">
+                      <h3
+                        className="flex gap-1 items-center"
+                        title={`Transmission: ${details.engine}`}
+                      >
+                        <IoMdSettings className="text-xl" />
+                        {details.engine}
+                      </h3>
+                      <h3 className="flex gap-1 items-center" title="CC">
+                        <FaCubes className="text-xl" />
+                        {details.cc}
+                      </h3>
+
+                      <h3 className="flex gap-1 items-center" title="Top Speed">
+                        <BsSpeedometer2 className="text-xl" />
+                        {details.top_speed} Km/h
+                      </h3>
+                      <h3 className="flex gap-1 items-center" title="Mileage">
+                        <GiPathDistance className="text-xl" />
+                        {details.mileage} Km
+                      </h3>
                     </div>
                   </div>
 
